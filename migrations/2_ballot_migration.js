df2df2543bbe03e0bdf8ede448df4bb86af3fdf6
Gurkirat Singh <tbhaxor@gmail.com>
2022-03-03 05:12:14 +0530

    remove dotenv from ballot migration

--- migrations/2_ballot_migration.js
+++ migrations/2_ballot_migration.js
@@ -1,5 +1,4 @@
 const Ballot = artifacts.require("Ballot");
-require("dotenv").config()
 
 module.exports = function (deployer) {
   deployer.deploy(Ballot, parseInt(process.env.MAX_PROPOSALS || "3"));
