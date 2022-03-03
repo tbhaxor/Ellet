280fea81b8f295c3173394fced8f1a4df6e678dd
Gurkirat Singh <tbhaxor@gmail.com>
2022-03-04 02:20:34 +0530

    configure webpack fallbacks for browser pollyfill

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require("webpack")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"],
+        process: "process/browser",
+      }),
+    ],
+    resolve: {
+      fallback: {
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        os: require.resolve("os-browserify/browser"),
+        url: require.resolve("url"),
+        assert: require.resolve("assert"),
+      },
+    },
+  }
 })
