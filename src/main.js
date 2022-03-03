fb2716f2d0bc990612bf76065298904698e6b6aa
Gurkirat Singh <tbhaxor@gmail.com>
2022-03-04 02:02:42 +0530

    add web3 v2 alpha

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Web3 from "web3"
+
+const httpWeb3 = new Web3.providers.HttpProvider("http://127.0.0.1:9545/")
+Vue.prototype.$web3 = new Web3(httpWeb3)
 
 Vue.config.productionTip = false
 
