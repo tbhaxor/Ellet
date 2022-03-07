5683bd1a026b643f543f8cd2304baf703a22395e
Gurkirat Singh <tbhaxor@gmail.com>
2022-03-07 09:46:28 +0530

    add registeration component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Ellet Ballot" />
+    <HelloWorld msg="Welcome to Ellet" />
+
+    <Views />
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import HelloWorld from "./components/HelloWorld.vue";
+import Views from "@/components/views/Index.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Views,
   },
   created() {
     // get all the addresses on the chain
@@ -84,6 +88,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="page" />
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Register from "./Registration.vue";
+export default {
+  components: { Register },
+  computed: {
+    ...mapGetters(["page"]),
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin-top: 10px;
+}
+</style>
